c6a8d6161fce901c4226f6e945ad5e7943a6a78c
gmii <gumilarharisubagja28@gmail.com>
2025-03-24 02:15:17 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300137/Program.cs
+++ tpmodul6_103022300137/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul6_103022300137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - GUMILAR HARI SUBAGJA");
+            for (int i = 0; i < 5; i++)  
+            {
+                video.IncreasePlayCount(2000000);
+            }
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ tpmodul6_103022300137/SayaTubeVideo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace tpmodul6_103022300137
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Debug.Assert(title != null && title.Length <= 100, "Judul tidak valid");
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count <= 10000000 && count >= 0, "Penambahan play count terlalu besar atau negatif");
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Error: terjadi overflow saat menambahkan play count.");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Judul: " + title);
+            Console.WriteLine("Jumlah view: " + playCount);
+        }
+    }
+}
